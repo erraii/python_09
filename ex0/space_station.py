6934d30569c904604221fe744c1d24ce197eb575
erraii <eraycakiray@gmail.com>
2026-06-29 23:59:01 +0200

    Implement SpaceStation class with data validation
    This script defines a SpaceStation class using Pydantic for data validation, including attributes like station_id, name, crew_size, power_level, and oxygen_level. It also includes a main function that creates instances of SpaceStation and handles validation errors.

---
+++ ex0/space_station.py
@@ -0,0 +1,105 @@
+from pydantic import BaseModel, Field, ValidationError
+from datetime import datetime
+
+
+class SpaceStation(BaseModel):
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length=50)
+    crew_size: int = Field(ge=1, le=20)
+    power_level: float = Field(ge=0.0, le=100.0)
+    oxygen_level: float = Field(ge=0.0, le=100.0)
+    last_maintenance: datetime
+    is_operational: bool = True
+    notes: str | None = Field(default=None, max_length=200)
+
+
+def print_station(station: SpaceStation) -> None:
+    print("Valid station created:")
+    print(f"ID: {station.station_id}")
+    print(f"Name: {station.name}")
+    print(f"Crew: {station.crew_size} people")
+    print(f"Power: {station.power_level}%")
+    print(f"Oxygen: {station.oxygen_level}%")
+    # print(f"Last Maint.: {station.last_maintenance.strftime('%d/%m/%Y')}")
+    if station.is_operational:
+        print("Status: Operational")
+    else:
+        print("Status: Maintenance")
+    if station.notes:
+        print(f"notes: {station.notes}")
+
+
+def main() -> None:
+    print("\nSpace Station Data Validation")
+    print("========================================")
+
+    # STATION 1 - Valid Data
+    try:
+        station2 = SpaceStation(
+                station_id="ISS001",
+                name="International Space Station",
+                crew_size=6,
+                power_level=85.5,
+                oxygen_level=92.3,
+                last_maintenance=datetime.now(),
+                # last_maintenance="2026-06-29T00:00:00"
+                )
+        print_station(station2)
+        print("\n========================================")
+
+    except ValidationError as error:
+        print("Expected validation error:")
+        print(error.errors()[0]["msg"])
+
+    # STATION 2 - Invalid data
+    try:
+        station3 = SpaceStation(
+                station_id="TMO001",
+                name="Titan Mining Outpost",
+                # crew_size=20,
+                crew_size=26,
+                power_level=95.5,
+                # oxygen_level=62.3,
+                oxygen_level=120.9,
+                last_maintenance=datetime.now(),
+                # last_maintenance="2026-04-25T00:00:00"
+                )
+        print_station(station3)
+        print("\n========================================")
+
+    except ValidationError as error:
+        print("Expected validation error:")
+        print(error.errors()[0]["msg"])
+
+    # # STATION 3 - Valid, attribute init with with dictionary
+    # try:
+    #     external_data = {
+    #         "station_id": "QCH001",
+    #         "name": "Quantum Communications Hub",
+    #         "crew_size": 16,
+    #         "power_level": 55.1,
+    #         "oxygen_level": 99.3,
+    #         "last_maintenance": datetime.now(),
+    #         # "last_maintenance": "2024-02-17T00:00:00",
+    #         "is_operational": False
+    #     }
+    #     station1 = SpaceStation(**external_data)
+    #     print_station(station1)
+    #     print("\n========================================")
+
+    # except ValidationError as error:
+    #     print("Expected validation error:")
+    #     print(error.errors()[0]["msg"])
+
+
+if __name__ == '__main__':
+
+    # create a virtaul env first:
+    # virtualenv space_env
+    # and then activate
+    # source space_env/bin/activate
+    # install pydantic:
+    # pip install pydantic
+    # validate the version:
+    # pip show pydantic
+    main()
